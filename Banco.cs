e1609cdb22145760de8315d94cfb85e81caa8dbb
Frenny De La Cruz <102378124+frennysmith15@users.noreply.github.com>
2024-02-01 15:50:40 -0400

    Done

---
+++ Banco.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class Banco
+{
+	private double dineroTotal;
+	private Cliente cliente1, cliente2, cliente3;
+
+	public Banco(Cliente cliente1, Cliente cliente2, Cliente cliente3, )
+	{
+		this.cliente1 = cliente1;
+		this.cliente2 = cliente2;
+		this.cliente3 = cliente3;
+		this.dineroTotal = cliente1.getDeposito() + cliente2.getDeposito() + cliente3.getDeposito();
+	}
+
+	public double getDineroTotal() {  return dineroTotal; }
+}
+
+
+public class Cliente
+{
+	private string nombre;
+	private string apellido;
+	private double deposito;
+
+
+	public Cliente(string nombre, string apellido, double deposito)
+	{
+		this.nombre = nombre;
+		this.apellido = apellido; 
+		this.deposito = deposito;
+	}
+
+	public void setDeposito(double deposito)
+	{
+		this.deposito = deposito;
+	}
+
+	public double getDeposito()
+	{
+		return this.deposito;
+	}
+
+}
